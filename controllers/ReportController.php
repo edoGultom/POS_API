fd9611135135a3575f7b1aed16308a34628cde88
edoGultom <edogultom10395@gmail.com>
2024-06-15 10:52:18 +0700

    report by date

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -44,6 +44,7 @@ class ReportController extends Controller
             'verbs' => [
                 'class' => VerbFilter::class,
                 'actions' => [
+                    'by-date-range'  => ['POST'],
                     'by-date'  => ['POST'],
                 ],
             ],
@@ -79,6 +80,36 @@ class ReportController extends Controller
         )->queryAll();
         return array_values($model);
     }
+    protected function findByDate($date)
+    {
+        $model = Yii::$app->db->createCommand(
+            "
+                SELECT 
+                    to_char(to_timestamp(s.created_at), 'YYYY-mm-dd') AS date,
+                    p.id, -- ID produk
+                    p.nama_barang, -- Nama produk
+                    SUM(si.qty) AS total_quantity_sold, -- Total kuantitas yang terjual
+                    SUM(si.harga * si.qty) AS total_sales_amount, -- Total penjualan dalam nilai uang
+                    SUM(CASE WHEN py.payment_method = 'CASH' THEN si.harga * si.qty ELSE 0 END) AS cash_sales, -- Total penjualan tunai
+                    SUM(CASE WHEN py.payment_method = 'QRIS' THEN si.harga * si.qty ELSE 0 END) AS qris_sales -- Total penjualan via QRIS
+                FROM 
+                    tbl_penjualan s
+                JOIN 
+                    tbl_penjualan_barang si ON s.id = si.id_penjualan -- Gabung tabel Sales dan Sale_Items
+                JOIN 
+                    tbl_barang p ON si.id_barang = p.id -- Gabung tabel Sale_Items dan Products
+                LEFT JOIN 
+                    tbl_pembayaran py ON s.id = py.id_penjualan -- Gabung tabel Sales dan Payments
+                WHERE 
+                     to_char(to_timestamp(s.created_at), 'YYYY-mm-dd') ='$date'
+                GROUP BY 
+                    to_char(to_timestamp(s.created_at), 'YYYY-mm-dd'), p.id, p.nama_barang -- Kelompokkan berdasarkan tanggal, ID produk, dan nama produk
+                ORDER BY 
+                    to_char(to_timestamp(s.created_at), 'YYYY-mm-dd'), p.id; -- Urutkan berdasarkan tanggal dan ID produk
+            "
+        )->queryAll();
+        return array_values($model);
+    }
     protected function findAllModel()
     {
         $model = TblBarang::find()->all();
@@ -87,7 +118,7 @@ class ReportController extends Controller
         }
         throw new NotFoundHttpException('Data Tidak Ditemukan.');
     }
-    public function actionByDate()
+    public function actionByDateRange()
     {
         $res = [];
         $connection = Yii::$app->db;
@@ -96,6 +127,35 @@ class ReportController extends Controller
         $dateStart = $data['start'];
         $dateEnd = $data['end'];
         $model = $this->findByDateRange($dateStart, $dateEnd);
+
+        $transaction = $connection->beginTransaction();
+        try {
+            if ($model) {
+                $transaction->commit();
+                $res['status'] = true;
+                $res['data'] = $model;
+                $res['message'] = 'Berhasil mengambil data!';
+            } else {
+                $res['status'] = true;
+                $res['message'] = 'Data Kosong';
+            }
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            return [
+                'status' => false,
+                'message' => $e->getMessage(),
+            ];
+        }
+        return $res;
+    }
+    public function actionByDate()
+    {
+        $res = [];
+        $connection = Yii::$app->db;
+        $request = Yii::$app->request;
+        $data = $request->bodyParams; // Get the body of the request
+        $date = $data['date'];
+        $model = $this->findByDate($date);
 
         $transaction = $connection->beginTransaction();
         try {
