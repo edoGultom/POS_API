5a6b21f66a614aa779d0252547197ea2115ca64e
edoGultom <edogultom10395@gmail.com>
2024-06-26 21:54:05 +0700

    perbaikan

--- controllers/PembayaranController.php
+++ controllers/PembayaranController.php
@@ -162,7 +162,7 @@ class PembayaranController extends Controller
                 if ($midtransResp->status_code == '201') {
                     $pembayaran->payment_gateway = 'midtrans';
                     $pembayaran->id_transaksi = $midtransResp->transaction_id;
-                    $actions = json_decode($midtransResp, true);
+                    $actions = json_decode($midtransResp);
                     return $actions;
                     $pembayaran->link_qris = $midtransResp->actions[0]['url'];
                     if (!$pembayaran->save()) {
